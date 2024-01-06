cc4b056d843fa3f125df40ef89b64b71dc2fa54d
Felix Owolabi <owolabifelix78@gmail.com>
2024-01-06 11:52:45 +0100

    MUI - Button

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,7 +1,17 @@
-import { Button, Stack } from '@mui/material'
-import React from 'react'
+import { Button, ButtonGroup, IconButton, Stack, ToggleButton, ToggleButtonGroup } from '@mui/material'
+import React, { useState } from 'react'
+import SendIcon from '@mui/icons-material/Send';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
 
 const MuiButton = () => {
+  const [formats, setFormats] = useState<string[]>([])
+
+  console.log(formats)
+  const handleChangeFormat=(e:React.MouseEvent<HTMLElement>,newValue: string[])=>{
+    setFormats(newValue)
+  }
   return (
     <>
      <h2>Buttons:</h2>
@@ -14,8 +24,8 @@ const MuiButton = () => {
     <Stack spacing={2} direction='row'>
       <Button variant='contained' color='primary'>Primary Button</Button>
       <Button variant='contained' color='secondary'>Secondary Button</Button>
-      <Button variant='contained' color='error'>Error Button</Button>
-      <Button variant='contained' color='warning'>Warning Button</Button>
+      <Button variant='contained' color='error' disableRipple>Error Button</Button>
+      <Button variant='contained' color='warning' disableElevation>Warning Button</Button>
       <Button variant='contained' color='info'>Info Button</Button>
       <Button variant='contained' color='success'>Success Button</Button>
     </Stack>
@@ -23,6 +33,27 @@ const MuiButton = () => {
      <Button variant='contained' size='small'>Small</Button>
      <Button variant='contained' size='medium'>Medium</Button>
      <Button variant='contained' size='large'>Large</Button>
+    </Stack>
+    <Stack spacing={2} direction='row'>
+     <Button variant='contained' startIcon={<SendIcon/>}>Send</Button>
+     <Button variant='contained' endIcon={<SendIcon/>}>Send</Button>
+     <IconButton color='success'  size='small' onClick={()=> alert('Working!')} >
+      <SendIcon/>
+     </IconButton>
+    </Stack>
+    <Stack direction='row'>
+      <ButtonGroup variant='contained' size='medium' color='error' orientation='vertical'>
+        <Button>Left</Button>
+        <Button>Center</Button>
+        <Button>Right</Button>
+      </ButtonGroup>
+    </Stack>
+    <Stack>
+      <ToggleButtonGroup aria-label='text formatting' color='primary' value={formats} onChange={handleChangeFormat}>
+        <ToggleButton aria-label='bold' value='bold'><FormatBoldIcon/></ToggleButton>
+        <ToggleButton aria-label='italic' value='italic'><FormatItalicIcon/></ToggleButton>
+        <ToggleButton aria-label='underline'  value='underlined'><FormatUnderlinedIcon/></ToggleButton>
+      </ToggleButtonGroup>
     </Stack>
      </Stack>
     </>
