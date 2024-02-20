40eea3719e6c9caa7e016bd1393645e8bfc3359f
Felix Owolabi <owolabifelix78@gmail.com>
2024-02-20 12:09:17 +0100

    MuiLayout

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,9 +1,9 @@
-import { Box, Stack,Divider,Grid } from '@mui/material';
+import { Box, Stack,Divider,Grid,Paper } from '@mui/material';
 import React from 'react';
 
 const MuiLayout = () => {
   return (
-    <>
+    <Paper sx={{backgroundColor:'#e3e3e3',margin: '0 auto',width:'80%',padding:'1rem'}} elevation={4} >
     <Stack sx={{border: '1px solid '}} direction={'row'} divider={<Divider orientation='vertical' flexItem />} >
     <Box component={'header'} sx={{
         backgroundColor: 'primary.main',
@@ -23,7 +23,7 @@ const MuiLayout = () => {
         <Grid item xs={3}><Box bgcolor='warning.light' p={2}>Item 3</Box></Grid>
         <Grid item xs={9}><Box bgcolor='info.light' p={2}> Item 4</Box></Grid>
     </Grid>
-    </>
+    </Paper>
 
 
   )
